03fd2d71bec6f50451ca38cd7f0716b844ce7000
Sergey Kogan <sekogan@gmail.com>
2014-12-14 13:30:05 +0300

    Make tests more strict, check exact bounds of scopes. Add lists to syntax file.

--- tests/syntax_test.py
+++ tests/syntax_test.py
@@ -1,19 +1,11 @@
 import sublime
 import unittest
 
-SINGLE_SCOPE = 1
-
-def make_human_readable(string):
-    return (string
-        .replace('\n', r'\n')
-        .replace('\r', r'\r')
-        .replace('\t', r'\t')
-        )
-
 class SyntaxTestCase(unittest.TestCase):
     def setUp(self):
         self.view = sublime.active_window().new_file()
         self.view.settings().set('auto_indent', False)
+        self.reset_scopes()
 
     def tearDown(self):
         if self.view:
@@ -24,60 +16,197 @@ class SyntaxTestCase(unittest.TestCase):
     def set_syntax_file(self, syntax_file):
         assert(self.view)
         self.view.set_syntax_file(syntax_file)
+        self.reset_scopes()
 
     def set_text(self, string):
         self.view.run_command("select_all")
         self.view.run_command("left_delete")
         self.view.run_command("insert", {"characters": string})
+        self.reset_scopes()
 
-    def set_default_scope(self, default_scope):
-        self.default_scope = default_scope
+    def check_in_scope(self, patterns, scope):
+        """
+        Checks that region(s) found by pattern(s) lie(s) within
+        the specified scope.
+        """
+        self.check_patterns(patterns, scope, self.in_scope)
 
-# TODO:
-# 1. If region.begin() has scopes A and B, but the rest
-# of the text has only scope B then extract_scope(begin)
-# will select only region.begin
-# 2. Add methods of checking that given region
-# - has scope
-# - has one scope
-# - has one scope and scope has exactly the same region
-# - has scope and scope has exactly the same region
-# 3. Use extract_scope to find positions to check.
+    def check_in_single_scope(self, patterns, scope):
+        """
+        Checks that region(s) found by pattern(s) lie(s) within
+        the specified scope and no other scopes
+        are present in that region(s).
+        """
+        self.check_patterns(patterns, scope, self.in_single_scope)
 
-    def has_scope(self, region, scope):
-        pos = region.begin()
-        scope_region = self.view.extract_scope(pos)
-        if not scope_region.contains(region):
-            return False
-        return self.view.score_selector(pos, scope) > 0
+    def check_eq_scope(self, patterns, scope):
+        """
+        Checks that begin and end positions of region(s)
+        found by pattern(s) are equal to begin and end
+        positions of the specified scope.
+        """
+        self.check_patterns(patterns, scope, self.eq_scope)
 
-    def has_single_scope(self, region, scope):
-        if not self.has_scope(region, scope):
-            return False
-        begin_scope = self.view.scope_name(region.begin())
-        scopes = map(lambda pos: self.view.scope_name(pos),
-            range(region.begin() + 1, region.end()))
-        return (len(begin_scope.split()) == 1 and
-            all(map(lambda scope: scope == begin_scope, scopes)))
+    def check_eq_single_scope(self, patterns, scope):
+        """
+        Checks that begin and end positions of region(s)
+        found by pattern(s) are equal to begin and end
+        positions of the specified scope and no other
+        scopes are present in that regions.
+        """
+        self.check_patterns(patterns, scope, self.eq_single_scope)
 
-    def verify_pattern(self, pattern, scope, flags):
-        regions = self.view.find_all(pattern)
-        self.assertTrue(regions, 'Cannot find pattern /{}/'.format(pattern))
-        check = self.has_single_scope if flags == SINGLE_SCOPE else self.has_scope
-        for region in regions:
-            self.assertTrue(check(region, scope),
-                'Text "{}" found by /{}/ does not match scope "{}"'.format(
-                    make_human_readable(self.view.substr(region)),
-                    pattern,
-                    scope
-                    )
-                )
+    def check_no_scope(self, patterns, scope):
+        """
+        Checks that region(s) found by pattern(s) doesn't/don't
+        contain subregions with the specified scope.
+        """
+        self.check_patterns(patterns, scope, self.no_scope)
 
-    def verify_scope(self, patterns, scope, flags = 0):
+    # ------------------------------------------------------
+
+    def check_patterns(self, patterns, scope, check):
         if type(patterns) is not list:
             patterns = [ patterns ]
         for pattern in patterns:
-            self.verify_pattern(pattern, scope, flags)
+            self.check_pattern(pattern, scope, check)
 
-    def verify_default(self, patterns):
-        self.verify_scope(patterns, self.default_scope, SINGLE_SCOPE)
+    def check_pattern(self, pattern, scope, check):
+        regions = self.view.find_all(pattern)
+        self.assertTrue(regions, 'Cannot find pattern /{}/'.format(pattern))
+        for region in regions:
+            result = check(region, scope)
+            self.assertTrue(result.passed(),
+                'Text "{}" at line {} found by /{}/ {}'.format(
+                    self.text(region),
+                    self.view.rowcol(region.begin())[0] + 1,
+                    pattern,
+                    result.reason()
+                    )
+                )
+
+    def in_scope(self, region, scope):
+        scope_region = self.scopes().find_first(scope, region);
+        if scope_region.empty():
+            return CheckFailed('should be in scope "{}" but no such scope found'.format(scope))
+        if not scope_region.contains(region):
+            return CheckFailed(
+                'should be in scope "{}" but that scope covers only "{}"'.format(
+                    scope, self.text(scope_region)
+                ))
+        return CheckPassed()
+
+    def in_single_scope(self, region, scope):
+        if self.scopes().number_of_scopes(region) != 1:
+            return CheckFailed('should have single scope "{}" but multiple scopes found'.format(scope))
+        return self.in_scope(region, scope)
+
+    def eq_scope(self, region, scope):
+        scope_region = self.scopes().find_first(scope, region);
+        if scope_region.empty():
+            return CheckFailed('should have scope "{}" but no such scope found'.format(scope))
+        if scope_region != region:
+            return CheckFailed(
+                'should have scope "{}" but that scope covers different region "{}"'.format(
+                scope, self.text(scope_region)
+                ))
+        return CheckPassed()
+
+    def eq_single_scope(self, region, scope):
+        if self.scopes().number_of_scopes(region) != 1:
+            return CheckFailed('should have single scope "{}" but multiple scopes found'.format(scope))
+        return self.eq_scope(region, scope)
+
+    def no_scope(self, region, scope):
+        scope_region = self.scopes().find_first(scope, region);
+        if not scope_region.empty():
+            rowcol = self.view.rowcol(scope_region.begin())
+            return CheckFailed('should not have scope "{}" but it was found at line {} column {}'.format(
+                scope, rowcol[0], rowcol[1]
+                ))
+        return CheckPassed()
+
+    def scopes(self):
+        if not self._scopes:
+            self._scopes = Scopes(self.view)
+        return self._scopes
+
+    def reset_scopes(self):
+        self._scopes = None
+
+    def text(self, region):
+        s = self.view.substr(region)
+        return (s
+            .replace('\n', r'\n')
+            .replace('\r', r'\r')
+            .replace('\t', r'\t')
+            )     
+
+
+class Scopes:
+    EMPTY_REGION = sublime.Region(0,0)
+
+    def __init__(self, view):
+        self.scopes = self._make_scopes_list(view)
+
+    def find_first(self, scope_prefix, region):
+        scopes = self._find_by_region(region)
+        for scope in scopes:
+            if scope['name'].startswith(scope_prefix):
+                return scope['region']
+        return self.EMPTY_REGION
+
+    def number_of_scopes(self, region):
+        return len(list(self._find_by_region(region)))
+
+    def _find_by_region(self, region):
+        return filter(lambda scope: (
+            scope['region'].intersects(region)
+            ), self.scopes)
+
+    @staticmethod
+    def _make_scopes_list(view):
+        scopes = []
+        begin = dict()
+        open_scopes = set()
+        for pos in range(0, view.size()):
+            pos_scopes = set(view.scope_name(pos).split())
+            ended_scopes = open_scopes.difference(pos_scopes)
+            new_scopes = pos_scopes.difference(open_scopes)
+
+            for scope in new_scopes:
+                begin[scope] = pos
+
+            for scope in ended_scopes:
+                scopes.append({
+                    'name': scope,
+                    'region': sublime.Region(begin[scope],pos)
+                })
+
+            open_scopes = pos_scopes
+
+        for scope in open_scopes:
+            scopes.append({
+                'name': scope,
+                'region': sublime.Region(begin[scope],view.size())
+            })
+
+        return scopes
+
+
+class CheckResult:
+    def __init__(self, passed, reason = None):
+        self._passed = passed
+        self._reason = reason
+    def passed(self): return self._passed
+    def reason(self): return self._reason
+
+
+class CheckFailed(CheckResult):
+    def __init__(self, reason):
+        super().__init__(False, reason)
+
+
+class CheckPassed(CheckResult):
+    def __init__(self):
+        super().__init__(True)
--- tests/test_markdown_test_syntax.py
+++ tests/test_markdown_test_syntax.py
@@ -4,39 +4,43 @@ class TestMarkdownTestSyntax(syntax_test.SyntaxTestCase):
     def setUp(self):
         super().setUp()
         self.set_syntax_file("Packages/MarkdownTest/MarkdownTest.tmLanguage")
-        self.set_default_scope('text')
+
+    def check_default(self, patterns):
+        self.check_in_single_scope(patterns, 'text')
 
     def test_simple_text(self):
         self.set_text('A B C')
-        self.verify_default('A B C')
+        self.check_default('A B C')
 
     def test_italic(self):
         self.set_text('''
 A *B* _C_ D
 *E*
 ''')
-        self.verify_scope(list('BCE'), 'markup.italic')
-        self.verify_scope(r'[\*_]', 'punctuation.definition')
-        self.verify_default(list('AD '))
+        self.check_eq_scope(list('BCE'), 'markup.italic')
+        self.check_eq_scope(r'[\*_]', 'punctuation.definition')
+        self.check_default(list('AD '))
 
     def test_bold(self):
         self.set_text('''
 A **B** __C__ D
 **E**
 ''')
-        self.verify_scope(list('BCE'), 'markup.bold')
-        self.verify_scope(r'[\*_]', 'punctuation.definition')
-        self.verify_default(list('AD '))
+        self.check_eq_scope(list('BCE'), 'markup.bold')
+        self.check_eq_scope(r'[\*_]+', 'punctuation.definition')
+        self.check_default(list('AD '))
 
     def test_inline_markup_inside_inline_markup(self):
         self.set_text('''
 A *B **C** D* E
 F **G *H* I** J
 ''')
-        self.verify_scope(list('BCDH'), 'markup.italic')
-        self.verify_scope(list('CGHI'), 'markup.bold')
-        self.verify_scope(r'\*', 'punctuation.definition')
-        self.verify_default(list('AEFJ'))
+        self.check_eq_scope(r'B \*\*C\*\* D', 'markup.italic')
+        self.check_eq_scope(r'H', 'markup.italic')
+        self.check_eq_scope(r'C', 'markup.bold')
+        self.check_eq_scope(r'G \*H\* I', 'markup.bold')
+        self.check_eq_scope(r'\*+', 'punctuation.definition')
+        self.check_default(list('AEFJ'))
 
     def test_bold_italic(self):
         self.set_text('''
@@ -45,13 +49,17 @@ BA _**BB**_ BC
 CA **_CB_** CC
 DA __*DB*__ DC
 ''')
-        self.verify_scope(r'[A-Z]B', 'markup.bold')
-        self.verify_scope(r'[A-Z]B', 'markup.italic')
-        self.verify_scope(r'[\*_]', 'punctuation.definition')
-        self.verify_default([ r'[A-Z]A ', r' [A-Z]C', ' ' ])
+        self.check_eq_scope(r'__AB__', 'markup.italic')
+        self.check_eq_scope(r'\*\*BB\*\*', 'markup.italic')
+        self.check_eq_scope([ 'CB', 'DB' ], 'markup.italic')
+        self.check_eq_scope([ 'AB', 'BB' ], 'markup.bold')
+        self.check_eq_scope(r'_CB_', 'markup.bold')
+        self.check_eq_scope(r'\*DB\*', 'markup.bold')
+        self.check_eq_scope(r'\*+|_+', 'punctuation.definition')
+        self.check_default([ r'[A-Z]A ', r' [A-Z]C\n' ])
 
     def test_multiline_markup_not_supported(self):
-        # Multiline markup is not supported due to
+        # Multiline inline markup is not supported due to
         # limitations in syntax definition language.
         self.set_text('''
 A **B
@@ -59,54 +67,54 @@ C** D
 E _F
 G_ H
 ''')
-        self.verify_default('.')
+        self.check_default('.+')
 
     def test_inline_markup_before_punctuation(self):
         self.set_text('''
 A *B*: *C*; *D*, *E*. *F*? G
 K **L**: **M**; **N**, **O**. **P**? Q
 ''')
-        self.verify_scope(list('BCDEF'), 'markup.italic')
-        self.verify_scope(list('LMNOP'), 'markup.bold')
-        self.verify_scope(r'\*', 'punctuation.definition')
-        self.verify_default(r'[AGKQ:;,\.?]')
+        self.check_eq_scope(list('BCDEF'), 'markup.italic')
+        self.check_eq_scope(list('LMNOP'), 'markup.bold')
+        self.check_eq_scope(r'\*+', 'punctuation.definition')
+        self.check_default(r'[AGKQ:;,\.?]')
 
     def test_inline_markup_inside_quotes_and_brackets(self):
         self.set_text('''
 A "*B*" (*C*) '*D*' E
 K "**L**" (**M**) '**N**' O
 ''')
-        self.verify_scope(list('BCD'), 'markup.italic')
-        self.verify_scope(list('LMN'), 'markup.bold')
-        self.verify_scope(r'\*', 'punctuation.definition')
-        self.verify_default(r'''[AEKQ"\(\)'\.]''')
+        self.check_eq_scope(list('BCD'), 'markup.italic')
+        self.check_eq_scope(list('LMN'), 'markup.bold')
+        self.check_eq_scope(r'\*+', 'punctuation.definition')
+        self.check_default(r'''[AEKQ"\(\)'\.]''')
 
     def test_brackets_inside_inline_markup(self):
         self.set_text('''
 *A (B C)*: D
 *(K)* **(L)**
 ''')
-        self.verify_scope([ r'A \(B C\)', r'\(K\)' ] , 'markup.italic')
-        self.verify_scope( r'\(L\)', 'markup.bold')
-        self.verify_scope(r'\*', 'punctuation.definition')
-        self.verify_default(r': D')
+        self.check_eq_scope([ r'A \(B C\)', r'\(K\)' ] , 'markup.italic')
+        self.check_eq_scope( r'\(L\)', 'markup.bold')
+        self.check_eq_scope(r'\*+', 'punctuation.definition')
+        self.check_default(r': D')
 
     def test_inline_markup_combinations(self):
         self.set_text('_A _ B_C D_E _ F_ *G* **H** <a>_I_</a>')
-        self.verify_scope([ 'A _ B_C D_E _ F',
+        self.check_eq_scope([ 'A _ B_C D_E _ F',
             'G', 'I' ], 'markup.italic')
-        self.verify_scope('H', 'markup.bold')
-        self.verify_default([ '<a>', '</a>' ])
+        self.check_eq_scope('H', 'markup.bold')
+        self.check_default([ '<a>', '</a>' ])
 
     def test_escaping_of_inline_punctuation(self):
         self.set_text(r'A *\*B\** C **D\*** E')
-        self.verify_scope(r'\\\*B\\\*', 'markup.italic')
-        self.verify_scope(r'D\\\*', 'markup.bold')
-        self.verify_default(list('ACE '))
+        self.check_eq_scope(r'\\\*B\\\*', 'markup.italic')
+        self.check_eq_scope(r'D\\\*', 'markup.bold')
+        self.check_default(list('ACE '))
 
-    def test_markup_does_not_work_inside_words(self):
+    def test_inline_markup_does_not_work_inside_words(self):
         self.set_text('A_B C_D_E')
-        self.verify_default('.+')
+        self.check_default('.+')
 
     def test_headings(self):
         self.set_text('''
@@ -123,10 +131,10 @@ G
 #### N ###########
 O
 ''')
-        self.verify_scope(list('ABCDEFKLMN'), 'entity.name.section')
-        self.verify_scope(list('ABCDEFKLMN# '), 'markup.heading')
-        self.verify_scope(r'#', 'punctuation.definition')
-        self.verify_default(list('GO'))
+        self.check_eq_scope(list('ABCDEFKLMN'), 'entity.name.section')
+        self.check_in_scope(list('ABCDEFKLMN# '), 'markup.heading')
+        self.check_eq_scope(r'#+', 'punctuation.definition')
+        self.check_default(list('GO'))
 
     def test_inline_markup_inside_headings(self):
         self.set_text('''
@@ -136,11 +144,13 @@ O
 #### K _L M_ N #
 Z
 ''')
-        self.verify_scope(list('ABCDEFKLMN_'), 'entity.name.section')
-        self.verify_scope(list('ABCDEFKLMN# '), 'markup.heading')
-        self.verify_scope(list('ACELM'), 'markup.italic')
-        self.verify_scope(r'#', 'punctuation.definition')
-        self.verify_default(r'Z')
+        self.check_eq_scope([
+            '_A_', 'B _C_', 'D _E_ F', 'K _L M_ N'
+            ], 'entity.name.section')
+        self.check_in_scope(list('ABCDEFKLMN#_ '), 'markup.heading')
+        self.check_eq_scope([ 'A', 'C', 'E', 'L M' ], 'markup.italic')
+        self.check_eq_scope(r'#+', 'punctuation.definition')
+        self.check_default(r'Z')
 
     def test_fenced_paragraph(self):
         self.set_text('''
@@ -152,9 +162,9 @@ A
 
 L
 ''')
-        self.verify_scope('A', 'markup.raw.block.fenced')
-        self.verify_scope('`', 'punctuation.definition')
-        self.verify_default([ r'K\n\n', r'\nL\n' ])
+        self.check_eq_scope(r'```\nA\n```\n', 'markup.raw.block.fenced')
+        self.check_eq_scope('`+', 'punctuation.definition')
+        self.check_default([ r'K\n\n', r'\nL\n' ])
 
     def test_fenced_block_inside_paragraph(self):
         self.set_text('''
@@ -166,9 +176,9 @@ A
 L
 
 ''')
-        self.verify_scope('A', 'markup.raw.block.fenced')
-        self.verify_scope('`', 'punctuation.definition')
-        self.verify_default([ r'\nK\n', r'L\n\n' ])
+        self.check_eq_scope(r'```\nA\n```\n', 'markup.raw.block.fenced')
+        self.check_eq_scope('`+', 'punctuation.definition')
+        self.check_default([ r'\nK\n', r'L\n\n' ])
 
     def test_syntax_highlighting_inside_fenced_blocks(self):
         self.set_text('''
@@ -180,10 +190,10 @@ def g():
     return 567
 ```
 ''')
-        self.verify_scope([ 'int', 'def' ], 'storage.type')
-        self.verify_scope([ '123', '567' ], 'constant.numeric')
-        self.verify_scope('g', 'entity.name')
-        self.verify_scope('return', 'keyword.control')
+        self.check_eq_scope([ 'int', 'def' ], 'storage.type')
+        self.check_eq_scope([ '123', '567' ], 'constant.numeric')
+        self.check_eq_scope('g', 'entity.name')
+        self.check_eq_scope('return', 'keyword.control')
 
     def test_indented_raw_blocks(self):
         self.set_text('''
@@ -193,16 +203,15 @@ A
 
 C
 ''')
-        self.verify_scope(r'    B\n', 'markup.raw.block')
-        self.verify_default([ r'\nA\n\n', r'\nC\n' ])
+        self.check_eq_scope(r'    B\n', 'markup.raw.block')
+        self.check_default([ r'\nA\n\n', r'\nC\n' ])
 
     def test_multiline_indented_raw_blocks(self):
         self.set_text('''
     A
     B
 ''')
-        self.verify_scope([ r'    A\n', r'    B\n' ],
-            'markup.raw.block')
+        self.check_eq_scope(r'    A\n    B\n', 'markup.raw.block')
 
     def test_indented_raw_blocks_glued_to_text(self):
         self.set_text('''
@@ -212,12 +221,12 @@ A
     C
 D
 ''')
-        self.verify_scope(r'    C\n', 'markup.raw.block')
-        self.verify_default([ r'\nA\n    B\n\n', r'D\n' ])
+        self.check_eq_scope(r'    C\n', 'markup.raw.block')
+        self.check_default([ r'\nA\n    B\n\n', r'D\n' ])
 
     def test_blank_line_is_not_indented_raw_block(self):
         self.set_text('\n\n    \n\n')
-        self.verify_default(r'\n[ ]+\n')
+        self.check_default(r'\n[ ]+\n')
 
     def test_inline_raw_text(self):
         self.set_text('''
@@ -225,23 +234,42 @@ A `B` C
 D`E`F
 K `L **M` N** O
 ''')
-        self.verify_scope(list('BE') + [ r'L \*\*M' ], 'markup.raw.inline')
-        self.verify_scope('`', 'punctuation.definition')
-        self.verify_default(list('ACDFK') + [ r' N\*\* O' ])
+        self.check_eq_scope(list('BE') + [ r'L \*\*M' ], 'markup.raw.inline.content')
+        self.check_eq_scope('`', 'punctuation.definition')
+        self.check_default(list('ACDFK') + [ r' N\*\* O' ])
 
     def test_incomplete_or_multiline_inline_raw_text(self):
         self.set_text('''
 A `B
 C` D
 ''')
-        self.verify_default('.+')
+        self.check_default('.+')
+
+    def test_multiple_backquotes_as_inline_raw_delimiters(self):
+        self.set_text('''
+``A``
+```B``
+``C```
+''')
+        self.check_eq_scope(list('AC'), 'markup.raw.inline.content')
+        self.check_eq_scope('`B', 'markup.raw.inline.content')
+        self.check_eq_scope([ r'^``', r'(?<=\w)``' ], 'punctuation.definition')
+        self.check_default([ r'(?<=C``)`', r'\n' ])        
+
+    def test_inline_raw_delimiters_does_not_start_fenced_block(self):
+        self.set_text('''
+```A```
+B
+''')
+        self.check_eq_scope('```A```', 'markup.raw.inline.markdown')
+        self.check_eq_scope('A', 'markup.raw.inline.content')
+        self.check_eq_scope('```', 'punctuation.definition')
+        self.check_default(r'B')        
 
     def test_quoted_text_alone(self):
-        self.set_text(
-'''>A
-''')
-        self.verify_scope(r'>A\n', 'markup.quote')
-        self.verify_scope(r'>', 'punctuation.definition')
+        self.set_text('>A\n')
+        self.check_eq_scope(r'>A\n', 'markup.quote')
+        self.check_eq_scope(r'>', 'punctuation.definition')
 
     def test_one_line_quoted_block(self):
         self.set_text('''
@@ -249,9 +277,9 @@ C` D
 
 B
 ''')
-        self.verify_scope(r'>A\n', 'markup.quote')
-        self.verify_scope(r'>', 'punctuation.definition')
-        self.verify_default(r'\nB\n')
+        self.check_eq_scope(r'>A\n', 'markup.quote')
+        self.check_eq_scope(r'>', 'punctuation.definition')
+        self.check_default(r'\nB\n')
 
     def test_type_1_multiline_quoted_block(self):
         self.set_text('''
@@ -260,9 +288,9 @@ B
 
 C
 ''')
-        self.verify_scope(r'>A\nB\n', 'markup.quote')
-        self.verify_scope(r'>', 'punctuation.definition')
-        self.verify_default(r'\nC\n')
+        self.check_eq_scope(r'>A\nB\n', 'markup.quote')
+        self.check_eq_scope(r'>', 'punctuation.definition')
+        self.check_default(r'\nC\n')
 
     def test_type_2_multiline_quoted_block(self):
         self.set_text('''
@@ -271,9 +299,9 @@ C
 
 C
 ''')
-        self.verify_scope(r'>A\n>B\n', 'markup.quote')
-        self.verify_scope(r'>', 'punctuation.definition')
-        self.verify_default(r'\nC\n')
+        self.check_eq_scope(r'>A\n>B\n', 'markup.quote')
+        self.check_eq_scope(r'>', 'punctuation.definition')
+        self.check_default(r'\nC\n')
 
     def test_quoted_block_inside_paragraph(self):
         self.set_text('''
@@ -282,8 +310,8 @@ A
 
 C
 ''')
-        self.verify_scope(r'>B\n', 'markup.quote')
-        self.verify_default([ r'\nA\n', r'\nC\n' ])
+        self.check_eq_scope(r'>B\n', 'markup.quote')
+        self.check_default([ r'\nA\n', r'\nC\n' ])
 
     def test_spaces_before_and_after_quote_signs(self):
         self.set_text('''
@@ -293,9 +321,9 @@ C
 
 D
 ''')
-        self.verify_scope(r' > A\n {2}> {2}B\n {3}> {3}C\n', 'markup.quote')
-        self.verify_scope(r'>', 'punctuation.definition')
-        self.verify_default(r'\nD\n')
+        self.check_eq_scope(r' > A\n {2}> {2}B\n {3}> {3}C\n', 'markup.quote')
+        self.check_eq_scope(r'>', 'punctuation.definition')
+        self.check_default(r'\nD\n')
 
     def test_inline_markup_inside_quoted_text(self):
         self.set_text('''
@@ -303,16 +331,16 @@ D
 >  _B_
 > **C**
 ''')
-        self.verify_scope('A', 'markup.raw')
-        self.verify_scope('B', 'markup.italic')
-        self.verify_scope('C', 'markup.bold')
+        self.check_eq_scope('A', 'markup.raw.inline.content')
+        self.check_eq_scope('B', 'markup.italic')
+        self.check_eq_scope('C', 'markup.bold')
 
     def test_list_item_alone(self):
         self.set_text(
 '''- A
 ''')
-        self.verify_scope(r'- A\n', 'meta.paragraph.list')
-        self.verify_scope(r'-', 'punctuation.definition')
+        self.check_eq_scope(r'- A\n', 'meta.paragraph.list')
+        self.check_eq_scope(r'-', 'punctuation.definition')
 
     def test_multiline_list(self):
         self.set_text('''
@@ -321,61 +349,76 @@ D
 
 C
 ''')
-        # self.verify_scope(r'- A\n- B\n', 'meta.paragraph.list')
-        self.verify_scope(r'- B\n', 'meta.paragraph.list')
-        self.verify_scope(r'-', 'punctuation.definition')
-        self.verify_default(r'\nC\n')
+        self.check_eq_scope(r'- A\n- B\n', 'meta.paragraph.list')
+        self.check_eq_scope(r'-', 'punctuation.definition')
+        self.check_default(r'\nC\n')
 
-#     def test_type_1_multiline_quoted_block(self):
-#         self.set_text('''
-# >A
-# B
+    def test_different_types_of_unnumbered_list_bullets(self):
+        self.set_text('''
+- A
++ B
+* C
 
-# C
-# ''')
-#         self.verify_scope(r'>A\nB\n', 'markup.quote')
-#         self.verify_scope(r'>', 'punctuation.definition')
-#         self.verify_default(r'\nC\n')
+D
+''')
+        self.check_eq_scope(r'- A\n\+ B\n\* C\n', 'meta.paragraph.list')
+        self.check_eq_scope([ r'\+', r'\*', '-' ], 'punctuation.definition')
+        self.check_default(r'D')
 
-#     def test_type_2_multiline_quoted_block(self):
-#         self.set_text('''
-# >A
-# >B
+    def test_numbered_list(self):
+        self.set_text('''
+0. A
+1. B
+12345. C
 
-# C
-# ''')
-#         self.verify_scope(r'>A\n>B\n', 'markup.quote')
-#         self.verify_scope(r'>', 'punctuation.definition')
-#         self.verify_default(r'\nC\n')
+D
+''')
+        self.check_eq_scope(r'0\. A\n1\. B\n\d+\. C\n', 'meta.paragraph.list')
+        self.check_eq_scope([ r'0\.', r'1\.', '12345\.' ], 'punctuation.definition')
+        self.check_default(r'D')
 
-#     def test_quoted_block_inside_paragraph(self):
-#         self.set_text('''
-# A
-# >B
+    def test_nested_lists(self):
+        self.set_text('''
+- A
+ * B
+  + C
+   1. D
+2. E
 
-# C
-# ''')
-#         self.verify_scope(r'>B\n', 'markup.quote')
-#         self.verify_default([ r'\nA\n', r'\nC\n' ])
+Z
+''')
+        self.check_eq_scope(r'- A\n \* B\n  \+ C\n +1\. D\n2\. E\n', 'meta.paragraph.list')
+        self.check_eq_scope([ '-', r'\*', r'\+', r'1\.', r'2\.' ], 'punctuation.definition')
+        self.check_default('Z')
 
-#     def test_spaces_before_and_after_quote_signs(self):
-#         self.set_text('''
-#  > A
-#   >  B
-#    >   C
+    def test_spaces_after_bullet(self):
+        self.set_text('''
+-A
+- B
+-    C
 
-# D
-# ''')
-#         self.verify_scope(r' > A\n {2}> {2}B\n {3}> {3}C\n', 'markup.quote')
-#         self.verify_scope(r'>', 'punctuation.definition')
-#         self.verify_default(r'\nD\n')
+Z
+''')
+        self.check_eq_scope(r'- B\n- +C\n', 'meta.paragraph.list')
+        self.check_eq_scope([ r'-(?= B)', r'-(?= +C)' ], 'punctuation.definition')
+        self.check_default('Z')
 
-#     def test_inline_markup_inside_quoted_text(self):
-#         self.set_text('''
-# > `A`
-# >  _B_
-# > **C**
-# ''')
-#         self.verify_scope('A', 'markup.raw')
-#         self.verify_scope('B', 'markup.italic')
-#         self.verify_scope('C', 'markup.bold')
+    def test_list_inside_paragraph(self):
+        self.set_text('''
+A
+- B
+C
+''')
+        self.check_eq_scope(r'- B\n', 'meta.paragraph.list')
+        self.check_default([ r'\nA\n', r'C\n' ])
+
+    def test_inline_markup_inside_list_items(self):
+        self.set_text('''
+- `A`
+- _B_
+- **C**
+''')
+        self.check_in_scope(r'-.*$\n', 'meta.paragraph.list')
+        self.check_eq_scope('A', 'markup.raw.inline.content')
+        self.check_eq_scope('B', 'markup.italic')
+        self.check_eq_scope('C', 'markup.bold')
